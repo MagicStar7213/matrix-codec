82ae7f48dd7b4f47f51b885f190192fad5b62410
MagicStar7213 <87906759+MagicStar7213@users.noreply.github.com>
2024-12-10 13:57:06 +0000

    Added maintaining encoding matrix

--- main.py
+++ main.py
@@ -15,26 +15,29 @@ def encode(encodeMatrix: np.matrix=None) -> np.matrix:
       array_length=len(array)
       for i in range(order*order-array_length):
         array.append(0)
-    matrix=np.matrix(array).reshape(order, order).T
+    matrix=np.matrix(array, dtype=np.int64).reshape(order, order).T
     print(np.matmul(encodeMatrix, matrix))
-  if int(input("Do you want to keep the encoding matrix? Yes [1] or No [0] ")) == 1:
+  if input("Do you want to keep the encoding matrix? yes or no: ") == "yes":
     return encodeMatrix
   else:
     return None
 
 def decode(encodeMatrix: np.matrix=None) -> np.matrix:
-  matrix=np.matrix(input("Enter matrix numbers separating columns with , and rows with ;: "), dtype=np.int64)
+  matrices=input("Enter matrix numbers separating columns with , and rows with ; and separate matrices with spaces ( ): ")
   if encodeMatrix is None:
     encodeMatrix=np.matrix(input("Enter encoding matrix numbers separating columns with , and rows with ;: "), dtype=np.int64)
-  mat_message=np.matmul(encodeMatrix.getI(), matrix)
-  
-  abc=" ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  result = ""
-  for column in mat_message.T.tolist():
-    for it in column:
-      result+=abc[round(it)]
+  result=""
+  for matrix in matrices.split(' '):
+    mat_message=np.matmul(encodeMatrix.getI(), np.matrix(matrix, dtype=np.int64))
+    abc=" ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    temp_result = ""
+
+    for column in mat_message.T.tolist():
+      for it in column:
+        temp_result+=abc[round(it)]
+    result+=temp_result
   print(result)
-  if int(input("Do you want to keep the encoding matrix? Yes [1] or No [0] ")) == 1:
+  if input("Do you want to keep the encoding matrix? yes or no: ") == "yes":
     return encodeMatrix
   else:
     return None
@@ -53,13 +56,14 @@ if __name__ == "__main__":
   \____|  \___/   \__,_|  \___|  \___|   
                                          
 """)
+  return_value: np.matrix = None
   while (True):
-    option=input("Choose option: Encode [e] or Decode [d] ")
+    option=input("Choose option: Encode [e] or Decode [d]: ")
     if option == "d":
-      return_value: np.matrix = decode()
-      while (not(return_value is None)):
+      return_value = decode()
+      while input("Do you want to keep the decoding? yes or no: ") == "yes":
         return_value=decode(return_value)
     elif option == "e":
-      return_value: np.matrix = encode()
-      while not((return_value is None)):
+      return_value = encode()
+      while input("Do you want to keep the encoding? yes or no: ") == "yes":
         return_value=encode(return_value)
