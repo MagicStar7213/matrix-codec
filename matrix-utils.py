e0eb18a9fae3321e7600a3e1a6784433b67e7b86
MagicStar7213 <87906759+MagicStar7213@users.noreply.github.com>
2025-11-20 23:37:59 +0100

    Case checking does not depend on shape of matrix

--- matrix-utils.py
+++ matrix-utils.py
@@ -92,8 +92,9 @@ def rango():
     message:list[list[str]] = [m.split('/') for m in input("Introduce la matriz: ").split('//')] # Split string input into 2D array
     A = del_proportional_lines(del_zero_lines(list_to_matrix(message)))
     if A.is_symbolic():
+        minors_list: list[Matrix] = []
         if A.shape[0] != A.shape[1]:
-            minors_list: list[Matrix] = decompose_matrix([A])
+            minors_list = decompose_matrix([A])
             all_square = True
             for minor in minors_list:
                     if minor.shape[0] != minor.shape[1]:
@@ -106,53 +107,46 @@ def rango():
                         break
                 if not all_square:
                     minors_list = decompose_matrix(minors_list)
-            zero_values: list[Number] = []
-            for raw_minor in minors_list:
-                minor = del_proportional_lines(del_zero_lines(raw_minor))
-                if not Expr(minor.det()).is_number:
-                    for root in solve(minor.det()):
-                        sym = list(minor.free_symbols)[0]
-                        minors_affected = 1
-                        mins = minors_list.copy()
-                        mins.remove(minor)
-                        for m in mins:
-                            if Matrix(m.subs(sym, root)).det() == 0:
-                                minors_affected += 1
-                        if minors_affected == len(minors_list) and not root in zero_values:
-                            zero_values.append(root)
-            symbol = sorted(A.free_symbols)[0]
-            if zero_values:
+        else:
+            minors_list = [A]
+        zero_values: list[Number] = []
+        for raw_minor in minors_list:
+            minor = del_proportional_lines(del_zero_lines(raw_minor))
+            if not Expr(minor.det()).is_number:
+                for root in solve(minor.det()):
+                    sym = list(minor.free_symbols)[0]
+                    minors_affected = 1
+                    mins = minors_list.copy()
+                    mins.remove(minor)
+                    for m in mins:
+                        if Matrix(m.subs(sym, root)).det() == 0:
+                            minors_affected += 1
+                    if minors_affected == len(minors_list) and not root in zero_values:
+                        zero_values.append(root)
+        symbol = sorted(A.free_symbols)[0]
+        if zero_values:
+            try:
+                str1 = "[\'"
+                str2 = "\']"
+                print(f"Caso 1, si {str([f'{symbol} ≠ {x}' for x in zero_values]).removeprefix(str1).removesuffix(str2)}: \n Rango de A = {A.rank()}")
+            except ValueError:
+                print('ERROR: Mismatched dimensions.')
+                if input("Try again? [Y/n] ").lower() == "y":
+                    rango()
+                else:
+                    exit(1)
+            caso = 2
+            for root in zero_values:
                 try:
-                    str1 = "[\'"
-                    str2 = "\']"
-                    print(f"Caso 1, si {str([f'{symbol} ≠ {x}' for x in zero_values]).removeprefix(str1).removesuffix(str2)}: \n Rango de A = {A.rank()}")
+                    print(f"Caso {caso} si {symbol} = {root}:")
+                    print(f" Rango de A = {Matrix(A.subs(symbol, root)).rank(simplify=True)}")
                 except ValueError:
                     print('ERROR: Mismatched dimensions.')
                     if input("Try again? [Y/n] ").lower() == "y":
                         rango()
                     else:
                         exit(1)
-                caso = 2
-                for root in zero_values:
-                    try:
-                        print(f"Caso {caso} si {symbol} = {root}:")
-                        print(f" Rango de A = {Matrix(A.subs(symbol, root)).rank(simplify=True)}")
-                    except ValueError:
-                        print('ERROR: Mismatched dimensions.')
-                        if input("Try again? [Y/n] ").lower() == "y":
-                            rango()
-                        else:
-                            exit(1)
-                    caso += 1
-        else:
-            try:
-                print(f"El rango de A = {A.rank()}")
-            except ValueError:
-                print('ERROR: Mismatched dimensions.')
-                if input("Try again? [Y/n] ").lower() == "y":
-                    rango()
-                else:
-                    exit(1) 
+                caso += 1 
     else:
         try:
             print(f"El rango de A = {A.rank(iszerofunc=matrix_is_zero)}")
