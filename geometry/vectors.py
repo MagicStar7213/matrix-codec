b44a2c9d354bd7e203d2a3190c061dcb5b0f4def
MagicStar7213 <87906759+MagicStar7213@users.noreply.github.com>
2025-11-29 10:44:59 +0100

    Changed variable names and improved cross product representation

--- geometry/vectors.py
+++ geometry/vectors.py
@@ -4,21 +4,22 @@ from sympy.vector import CoordSys3D, Vector, matrix_to_vector
 def scalar():
     print('PRODUCTO ESCALAR')
     C = CoordSys3D('C')
-    v1: Vector = matrix_to_vector(Matrix([parse_expr(x, transformations='all') for x in input('Introduce el primer vector: ').split(',')]),C)
-    v2: Vector = matrix_to_vector(Matrix([parse_expr(x, transformations='all') for x in input('Introduce el segundo vector: ').split(',')]), C)
-    print(f'u\u2192 · v\u2192 = {pretty(v1.dot(v2))}')
+    u: Vector = matrix_to_vector(Matrix([parse_expr(x, transformations='all') for x in input('Introduce el primer vector: ').split(',')]),C)
+    v: Vector = matrix_to_vector(Matrix([parse_expr(x, transformations='all') for x in input('Introduce el segundo vector: ').split(',')]), C)
+    print(f'u\u2192 · v\u2192 = {pretty(u.dot(v))}')
 
 def vectorial():
     print('PRODUCTO VECTORIAL')
     C = CoordSys3D('C')
-    v1: Vector = matrix_to_vector(Matrix([parse_expr(x, transformations='all') for x in input('Introduce el primer vector: ').split(',')]),C)
-    v2: Vector = matrix_to_vector(Matrix([parse_expr(x, transformations='all') for x in input('Introduce el segundo vector: ').split(',')]), C)
-    print(f'u\u2192 \u2227 v\u2192 = {pretty(v1.cross(v2))}')
+    u: Vector = matrix_to_vector(Matrix([parse_expr(x, transformations='all') for x in input('Introduce el primer vector: ').split(',')]),C)
+    v: Vector = matrix_to_vector(Matrix([parse_expr(x, transformations='all') for x in input('Introduce el segundo vector: ').split(',')]), C)
+    u_v = u.cross(v).to_matrix(C)
+    print(f'u\u2192 \u2227 v\u2192 = ({u_v[0]}, {u_v[1]}, {u_v[2]})')
 
 def mix():
     print('PRODUCTO MIXTO')
     C = CoordSys3D('C')
-    v1: Vector = matrix_to_vector(Matrix([parse_expr(x, transformations='all') for x in input('Introduce el primer vector: ').split(',')]),C)
-    v2: Vector = matrix_to_vector(Matrix([parse_expr(x, transformations='all') for x in input('Introduce el segundo vector: ').split(',')]), C)
-    v3: Vector = matrix_to_vector(Matrix([parse_expr(x, transformations='all') for x in input('Introduce el tercer vector: ').split(',')]),C)
-    print(f'[u\u2192, v\u2192, w\u2192] = {pretty(v1.dot(v2.cross(v3)))}')
+    u: Vector = matrix_to_vector(Matrix([parse_expr(x, transformations='all') for x in input('Introduce el primer vector: ').split(',')]),C)
+    v: Vector = matrix_to_vector(Matrix([parse_expr(x, transformations='all') for x in input('Introduce el segundo vector: ').split(',')]), C)
+    w: Vector = matrix_to_vector(Matrix([parse_expr(x, transformations='all') for x in input('Introduce el tercer vector: ').split(',')]),C)
+    print(f'[u\u2192, v\u2192, w\u2192] = {pretty(u.dot(v.cross(w)))}')
