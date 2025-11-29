f888e6173be80643acd4ebcbe437062fd809d984
MagicStar7213 <87906759+MagicStar7213@users.noreply.github.com>
2025-11-29 16:23:14 +0100

    Changed structure to centralize running on main module but at the same time allow individual testing

---
+++ geometry/main.py
@@ -0,0 +1,42 @@
+if __name__ == '__main__':
+    from vectors import scalar, vectorial, mix
+else:
+    from .vectors import scalar, vectorial, mix
+
+
+def vectors():
+    print("""
+     _   _ _|_  _  ._  _ 
+ \\/ (/_ (_  |  (_) |  _\\ """)
+    while True:
+        option = input("Elige una opción: Producto escalar [s], Producto vectorial [v], Producto mixto [m] o Salir [q]: ")
+        match option:
+            case "s": scalar()
+            case "v": vectorial()
+            case "m": mix()
+            case "q": return
+            case _:
+                print('Error! Opción no disponible. Vuelve a intentarlo.')
+                continue
+
+def main():
+    print("""
+    ____                                _                
+    / ___|  ___   ___   _ __ ___    ___ | |_  _ __  _   _ 
+    | |  _  / _ \\ / _ \\ | '_ ` _ \\  / _ \\| __|| '__|| | | |
+    | |_| ||  __/| (_) || | | | | ||  __/| |_ | |   | |_| |
+    \\____| \\___| \\___/ |_| |_| |_| \\___| \\__||_|    \\__, |
+                                                    |___/ 
+    """)
+    while True:
+        option = input("Elige una opción: Geometría [g], Vectores [v] o Salir [q]: ")
+        match option:
+            case "g": pass
+            case "v": vectors()
+            case "q": break
+            case _:
+                print('Error! Opción no disponible. Vuelve a intentarlo.')
+                continue
+
+if __name__ == '__main__':
+    main()
---
+++ matrices/main.py
@@ -0,0 +1,66 @@
+if __name__ == '__main__':
+    from operations import determinante, adjunta, producto, inversa, rango
+    from codec import Main
+    import sympy
+    sympy.init_printing()
+else:
+    from .operations import determinante, adjunta, producto, inversa, rango
+    from .codec import Main
+
+
+def matrices():
+    print("""
+    __  __           _            _        
+    |  \\/  |   __ _  | |_   _ __  (_) __  __
+    | |\\/| |  / _` | | __| | '__| | | \\ \\/ /
+    | |  | | | (_| | | |_  | |    | |  >  < 
+    |_|  |_|  \\__,_|  \\__| |_|    |_| /_/\\_\\                                        
+    """)
+    while True:
+        option = input("Elige una opción: Adjunta [a], Codificador/decodificador [c], Determinante [d], Inversa [i], Producto [p], Rango [r] o Salir [q]: ")
+        match option:
+            case "d":
+                try:
+                    determinante()
+                except KeyboardInterrupt:
+                    print()
+                    continue
+            case "a":
+                try:
+                    adjunta()
+                except KeyboardInterrupt:
+                    print()
+                    continue
+            case "p":
+                try:
+                    producto()
+                except KeyboardInterrupt:
+                    print()
+                    continue
+            case "i":
+                try:
+                    inversa()
+                except KeyboardInterrupt:
+                    print()
+                    continue
+            case "r":
+                try:
+                    rango()
+                except KeyboardInterrupt:
+                    print()
+                    continue
+            case "q":
+                print("Saliendo...")
+                exit(0)
+            case "c":
+                try:
+                    Main().app()
+                except KeyboardInterrupt:
+                    print()
+                    continue
+            case _:
+                print('ERROR: No existe esa opción.')
+                continue
+
+if __name__ == '__main__':
+    matrices()
