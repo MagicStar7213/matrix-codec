3b953405bf2d22dd9c701c3dc881f53c03879b2f
MagicStar7213 <87906759+MagicStar7213@users.noreply.github.com>
2026-01-08 07:11:20 +0100

    Added parsing analytic geometry equations for points, lines and planes

---
+++ mathutils/geometry/geometry.py
@@ -0,0 +1,48 @@
+from sympy import parse_expr, Equality, Expr, Point, solve, Plane
+from sympy.abc import x, y ,z
+from sympy.parsing.sympy_parser import T
+import re
+
+
+class GeomPoint:
+    def __init__(self, name: str, p: Point):
+        self.name = name
+        self.point = p
+
+def str_to_list(raw: str) -> list[str | list]:
+    parsed = []
+    if re.match(r'([A-Z]+\(-?\d(\.\d+)?,-?\d(\.\d+)?(,-?\d(\.\d+)?)?\))', raw):
+        separated = raw.split('(')
+        separated[-1] = separated[-1].removesuffix(')')
+        parsed = [separated[0], list(map(parse_expr, separated[-1].split(',')))]
+        return parsed
+    for char in raw:
+        parsed.append(char)
+        if char == ':':
+            parsed[-1] = '='
+            parsed.append(raw[raw.index(char) + 1:].split(','))
+            break
+    return parsed
+
+def get_plane(eq: Equality) -> Plane:
+    p1 = Point(0, 0, solve(eq.subs(x, 0).subs(y, 0), z)[0])
+    return Plane(p1, normal_vector=(eq.lhs.coeff(x), eq.lhs.coeff(y), eq.lhs.coeff(z)))
+
+def parse_equations(raw: list[str | list]):
+    parsed = []
+    parsed += raw.copy()
+    if len(raw) == 2 and type(raw[0]) is str and type(raw[1]) is list and len(raw[1]) == 3 and all(isinstance(i, Expr) for i in raw[1]):
+        parsed.insert(-1, '=')
+        parsed[-1] = GeomPoint(raw[0], Point(*raw[1]))
+    else:
+        transformations = T[1:5]+T[6]+T[8]+T[7]+T[9:]
+        equations: list[Equality] = [parse_expr(eq, transformations=transformations).simplify() for eq in raw[-1]]
+        match len(equations):
+            case 1:
+                eq = equations[0]
+                parsed[-1] = get_plane(eq)
+            case 2:
+                parsed[-1] = get_plane(equations[0]).intersection(get_plane(equations[1]))[0]
+            case _:
+                raise ValueError('Objects defined by more than 2 equations are not supported')
+    return parsed
