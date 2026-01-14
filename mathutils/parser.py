c9caefc0f774c8ba4ac213771d0cdb95f28495ea
MagicStar7213 <87906759+MagicStar7213@users.noreply.github.com>
2026-01-14 17:59:03 +0100

    Changed ambiguous variable name "l" to lst

--- mathutils/parser.py
+++ mathutils/parser.py
@@ -3,16 +3,16 @@ import math
 import operator
 
 
-def are_elements_numbers(l: list[str]) -> bool:
-    for x in l:
+def are_elements_numbers(lst: list[str]) -> bool:
+    for x in lst:
         try:
             float(x)
         except ValueError:
             return False
     return True
 
-def construct_string(l: list[str | list]) -> str:
-    return ''.join([construct_string(x) if type(x) is list else x for x in l])
+def construct_string(lst: list[str | list]) -> str:
+    return ''.join([construct_string(x) if type(x) is list else x for x in lst])
 
 
 
