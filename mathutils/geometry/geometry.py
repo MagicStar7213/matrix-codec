43d9a73d138a5f023517a6b3c11a7307bd279260
MagicStar7213 <87906759+MagicStar7213@users.noreply.github.com>
2026-01-14 19:07:46 +0100

    Added specific casting to str

--- mathutils/geometry/geometry.py
+++ mathutils/geometry/geometry.py
@@ -32,7 +32,7 @@ def str_to_list(raw: str) -> list[str | list]:
     for char in raw:
         parsed.append(char)
         if char == ':':
-            parsed = [''.join(parsed[:-1]), '=']
+            parsed = [''.join(list(map(str, parsed[:-1]))), '=']
             parsed[-1] = '='
             parsed.append(raw[raw.index(char) + 1:].split(','))
             break
