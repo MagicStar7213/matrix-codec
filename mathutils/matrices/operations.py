899e50667fd1b43ab9ff93b740baf48592774bde
MagicStar7213 <87906759+MagicStar7213@users.noreply.github.com>
2026-01-18 21:02:24 +0100

    Added separate rank printing function

--- mathutils/matrices/operations.py
+++ mathutils/matrices/operations.py
@@ -44,65 +44,6 @@ def rank(A: Matrix, minors_list:list[Matrix] | None = None, unequalities: list[E
         return A.rank(iszerofunc=matrix_is_zero)
 
 
-
-def rank_per_symbol(A: Matrix, minors_list: list[Matrix], symbol: Symbol, symbols: list[Symbol], indents: int = 0):
-    indentation = "".join(["  " for _ in range(indents)])
-    zero_values: list[Expr] = []
-    for raw_minor in minors_list:
-        minor = del_proportional_lines(del_zero_lines(raw_minor))
-        if not Expr(minor.det()).is_number and symbol in Expr(minor.det()).free_symbols:
-            for root in solve(minor.det(), symbol):
-                minors_affected = 1
-                mins = minors_list.copy()
-                mins.remove(minor)
-                for m in mins:
-                    if Matrix(m.subs(symbol, root)).det() == 0 or not Expr(Matrix(m.subs(symbol, root)).det()).is_number:
-                        minors_affected += 1
-                if minors_affected == len(minors_list) and root not in zero_values:
-                    zero_values.append(root)
-    if zero_values:
-        str1 = "[\'"
-        str2 = "\']"
-        print(f"{indentation}Caso 1, si {str([f'{symbol} ≠ {x}' for x in zero_values]).removeprefix(str1).removesuffix(str2)}:")
-        syms_new = [x for x in symbols if x != symbol]
-        if syms_new:
-            rank_per_symbol(A,minors_list, syms_new[0], syms_new, indents+1)
-        else:
-            try:
-                print(f"{indentation}  El rango de A = {A.rank()}")
-            except ValueError:
-                print('ERROR: Mismatched dimensions.')
-                if input("Try again? [Y/n] ").lower() == "y":
-                    rango()
-                else:
-                    exit(1)
-        caso = 2
-        for root in zero_values:
-            print(f"{indentation}Caso {caso} si {symbol} = {root}:")
-            B = Matrix(A.subs(symbol, root))
-            syms_new: list[Symbol] = [x for x in symbols if x != symbol]
-            if syms_new:
-                rank_per_symbol(B,[minor.subs(symbol, root) for minor in minors_list], syms_new[0], syms_new, indents+1)
-            else:
-                try:
-                    print(f"  {indentation}El rango de A = {B.rank()}")
-                except ValueError:
-                    print('ERROR: Mismatched dimensions.')
-                    if input("Try again? [Y/n] ").lower() == "y":
-                        rango()
-                    else:
-                        exit(1)
-            caso += 1
-    else:
-        try:
-            print(f"{indentation}  El rango de A = {A.rank()}")
-        except ValueError:
-            print('ERROR: Mismatched dimensions.')
-            if input("Try again? [Y/n] ").lower() == "y":
-                rango()
-            else:
-                exit(1)
-
 def producto():
     print("PRODUCTO")
     print("Los elementos de la matriz deben separarse por / y cada fila con //")
@@ -191,24 +132,26 @@ def rango():
     print("Los elementos de la matriz deben separarse por / y cada fila con //")
     message:list[list[str]] = [m.split('/') for m in input("Introduce la matriz: ").split('//')] # Split string input into 2D array
     A = del_proportional_lines(del_zero_lines(list_to_matrix(message)))
-    if A.is_symbolic():
-        minors_list: list[Matrix] = []
-        if A.shape[0] != A.shape[1]:
-            minors_list = decompose_matrix([A])
-            while not all(minor.is_square for minor in minors_list):
-                square_minors = [minor for minor in minors_list if minor.is_square]
-                non_square_minors = [minor for minor in minors_list if not minor.is_square]
-                minors_list = decompose_matrix(non_square_minors) + square_minors
+    try:
+        ranks = rank(A)
+        print_rank(ranks)
+    except ValueError:
+        print("ERROR: Mismatched dimensions.")
+        if input("Try again? [Y/n] ").lower() == "y":
+            rango()
         else:
-            minors_list = [A]
-        symbols: list[Symbol] = list(ordered(A.free_symbols))
-        rank_per_symbol(A, minors_list, symbols[0], symbols)
+            exit(1)
+
+def print_rank(ranks: list[tuple[tuple[Symbol, Expr | list[Expr]], int | list]] | int, indents: int = 0):
+    indentation = "".join(["  " for _ in range(indents)])
+    pref = "["
+    suff = "]"
+    if isinstance(ranks, list):
+        caso_1 = ranks[0]
+        print(f"""{indentation}Caso 1, si {caso_1[0][0]} ≠ {str(caso_1[0][1]).removeprefix(pref).removesuffix(suff).replace("'","")}:""")
+        print_rank(caso_1[1],indents+1)
+        for caso in ranks[1:]:
+            print(f"{indentation}Caso {ranks.index(caso)+1} si {caso[0][0]} = {caso[0][1]}:")
+            print_rank(caso[1],indents+1)
     else:
-        try:
-            print(f"El rango de A = {A.rank(iszerofunc=matrix_is_zero)}")
-        except ValueError:
-            print('ERROR: Mismatched dimensions.')
-            if input("Try again? [Y/n] ").lower() == "y":
-                rango()
-            else:
-                exit(1)
+        print(indentation+f'El rango de A = {ranks}')
