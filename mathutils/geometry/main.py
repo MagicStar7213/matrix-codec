a772dfc4045c0ca49765e0a05fe987563d63807b
MagicStar7213 <87906759+MagicStar7213@users.noreply.github.com>
2026-02-16 12:06:31 +0100

    All user interface logic is handled here

--- mathutils/geometry/main.py
+++ mathutils/geometry/main.py
@@ -1,7 +1,12 @@
-from .vectors import main as vectors
-from .geometry import main as geometry
+import re
+
+from sympy import Equality, Expr, Line3D, Point3D, Symbol, Tuple, pretty
+
+from .geometry import VPlane, angle, distance, process_geometry, rel_pos, sim
+from .vectors import Vector
 
 def main():
+    env = {"classes": [Vector, VPlane, Line3D, Point3D], "whitelist": [], "vars": {}}
     print("""
      ____                                _                
     / ___|   ___   ___   _ __ ___    ___ | |_  _ __  _   _ 
@@ -11,16 +16,35 @@ def main():
                                                      |___/ 
     """)
     while True:
-        option = input("Elige una opción: Geometría [g], Vectores [v] o Salir [q]: ")
-        if option == 'g':  
-            geometry()
-        elif option == 'v':
-            vectors()
-        elif option == 'q':
-            break
+        raw = input(">> ")
+        if raw == 'q':
+            return
+        if raw.replace(" ", "") == "":
+            pass
+        elif re.match(r'relpos \w+,\w+(,\w+)?', raw):
+            env = rel_pos(raw, env)
+        elif re.match(r'< \w+,\w+', raw):
+            env = angle(raw, env)
+        elif re.match(r"d \w+,\w+", raw):
+            env = distance(raw, env)
+        elif re.match(r"sim \w+,\w+", raw):
+            env = sim(raw, env)
         else:
-            print('Error! Opción no disponible. Vuelve a intentarlo.')
-            continue
-
-if __name__ == '__main__':
-    main()
+            processed, env = process_geometry(raw, env)
+            if processed:
+                if processed in list(env['vars'].values()):
+                    ind = list(env["vars"].values()).index(processed)
+                    sym = pretty(Symbol(list(env['vars'].keys())[ind]))
+                    if isinstance(processed, (Line3D, VPlane)):
+                        eq = processed.equation()
+                        if isinstance(eq, Expr):
+                            eq = Equality(eq,0)
+                        elif isinstance(eq, Tuple):
+                            eq = tuple(Equality(i,0) for i in eq)
+                        print(f'{sym} ≡ {pretty(eq).replace("(","{").replace(")","}")}')
+                    elif isinstance(processed, Point3D):
+                        print(f'{sym}{processed.coordinates}')
+                    else:
+                        print(pretty(processed))
+                else:
+                    print(pretty(processed))
