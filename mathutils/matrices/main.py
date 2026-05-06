7e4c08a3294f7aaff5f40475cc5419e4e1155d2d
MagicStar7213 <87906759+MagicStar7213@users.noreply.github.com>
2026-05-06 21:49:34 +0200

    Removed all unused logic and updated range pattern

--- mathutils/matrices/main.py
+++ mathutils/matrices/main.py
@@ -1,12 +1,12 @@
 import regex as re
 
-from sympy import Add, Integer, Mul, NonSquareMatrixError, Rational, Symbol, factor, nsimplify, pprint
+from sympy import Add, Integer, Mul, Rational, Symbol, parse_expr, pprint
 
 from mathutils.parser import safe_eval
 from .codec import Main
 from .determinants import del_proportional_lines, del_zero_lines
 from .rank import print_rank, rank
-from .utils import MATRIX_PATTERN, Matrix, matrix_is_zero, parse_matrices
+from .utils import MATRIX_PATTERN, Matrix, parse_matrices
 
 
 def matrices():
@@ -26,36 +26,17 @@ def matrices():
             return
         elif raw.replace(" ","") == "codec":
             Main().app()
-        elif re.match(rf"adj (({MATRIX_PATTERN})|\w+)", raw):
-            A = parse_matrix(raw, 'adj ', env)
+        elif re.match(rf"(rg|rango|rank) ((\()|)({MATRIX_PATTERN}|[^()]+)(?(2)\)|)", raw):
+            A = parse_matrices(raw.replace('rg','').replace('rango','').replace('rank',''), env)
             if A:
-                try:
-                    adjugate = A.adjugate()
-                except NonSquareMatrixError:
-                    print("ERROR: Given matrix not square, thus ∄ adj A.")
-                else:
-                    print()
-                    pprint(factor(nsimplify(adjugate)))
-        elif re.match(rf"det (({MATRIX_PATTERN})|\w+)", raw) or re.match(rf"\|(({MATRIX_PATTERN})|\w+)\|", raw):
-            A = parse_matrix(raw, ['det ', '|'], env)
-            if A:
-                try:
-                    determinant = A.det(iszerofunc=matrix_is_zero)
-                except NonSquareMatrixError:
-                    print("ERROR: Given matrix not square, thus ∄ det A.")
-                else:
-                    pprint(factor(nsimplify(determinant)))
-        elif re.match(rf"(rg|rango|rank) (({MATRIX_PATTERN})|\w+)", raw):
-            A = parse_matrix(raw, ['rg ','rango ','rank '], env)
-            if A:
-                A = del_proportional_lines(del_zero_lines(A))
+                A = del_proportional_lines(del_zero_lines(parse_expr(A)))
                 try:
                     ranks = rank(A)
                     print_rank(ranks)
                 except ValueError:
                     print("ERROR: Mismatched dimensions.")
         else:
-            parsed = parse_matrices(raw).replace("^","**")
+            parsed = parse_matrices(raw, env).replace("^","**")
             try:
                 result, env = safe_eval(parsed, env)
             except (ValueError, NameError, TypeError, SyntaxError) as e:
@@ -64,19 +45,3 @@ def matrices():
                 if result:
                     print()
                     pprint(result)
-
-def parse_matrix(raw: str, op: str | list[str], env: dict) -> Matrix:
-    if isinstance(op, list):
-        processed = raw
-        for o in op:
-            processed = processed.replace(o, "")
-    else:
-        processed = raw.replace(op, "")
-    if processed in env["vars"]:
-        return env["vars"][processed]
-    else:
-        matrix, env = safe_eval(parse_matrices(processed), env)
-        if not matrix:
-            raise NameError("Matrix could not be found in stored variables nor parsed.")
-        else:
-            return matrix
